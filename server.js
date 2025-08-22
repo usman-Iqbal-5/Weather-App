66cab3b5b818f7bf63b68e7a1e1f356a45d413e3
usman-Iqbal-5 <usmaniqbal912@outlook.com>
2025-08-22 09:16:31 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,55 @@
+import bodyParser from "body-parser";
+import express from "express";
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
+const apiKey = process.env.APIKEY;
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/", (req, res)=>{
+    res.render("index.ejs");
+})
+
+app.post("/getWeather", async (req, res)=>{
+    const city = req.body.city;
+
+    const baseUrl = `https://api.openweathermap.org/data/2.5/forecast`;
+    const params = new URLSearchParams({
+        q : city,
+        appid: apiKey
+
+    })
+
+    const url = `${baseUrl}?${params.toString()}`
+    console.log(url)
+
+    try {
+        const responseJson = await fetch(url);
+
+        if (!responseJson.ok){
+            throw new Error(`${responseJson.status}`);
+        }
+
+        const response = await responseJson.json();
+        // console.log(response);
+        return res.render("index.ejs", {
+            content: response,
+            cityName: city
+        })
+        
+    } catch (error) {
+        // console.log(`${error.message}`);
+        res.render("index.ejs", {content: error.message});
+    }
+
+})
+
+app.listen(port, ()=>{
+    console.log(`sever is running on port ${port}`);
+})
+
